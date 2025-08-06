78bff02edf067777f243c834bdff0da19b02f333
yiuman6 <yaowen88.liang@gmail.com>
2025-08-06 10:19:59 +1000

    note

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -7,14 +7,18 @@ interface OrderItemInput {
 }
 
 interface CreateOrderInput {
-  userId?: number; // 可选
+  userId?: number;
   customerName: string;
   customerEmail: string;
   customerPhone: string;
   shippingAddress: string;
   position: string;
+  note?:string;
+  barcodeAll: boolean;         // ✅ 新增
+  packageType: "boxes" | "opp"; // ✅ 新增
   items: OrderItemInput[];
 }
+
 
 export const createOrderService = async (data: CreateOrderInput) => {
   const {
@@ -24,31 +28,37 @@ export const createOrderService = async (data: CreateOrderInput) => {
     customerPhone,
     shippingAddress,
     position,
+    note,
+    barcodeAll,
+    packageType,
     items
   } = data;
 
-  return prisma.order.create({
-    data: {
-      userId: userId ?? undefined, // 可选关联
-      customerName,
-      customerEmail,
-      customerPhone,
-      shippingAddress,
-      position,
-      items: {
-        create: items.map((item) => ({
-          product: { connect: { id: item.productId } },
-          quantity: item.quantity,
-        })),
+return prisma.order.create({
+  data: {
+    userId: userId ?? undefined,
+    customerName,
+    customerEmail,
+    customerPhone,
+    shippingAddress,
+    position,
+    note,
+    barcodeAll,     // ✅ 新增
+    packageType,    // ✅ 新增
+    items: {
+      create: items.map((item) => ({
+        product: { connect: { id: item.productId } },
+        quantity: item.quantity,
+      })),
+    },
+  },
+  include: {
+    user: true,
+    items: {
+      include: {
+        product: true,
       },
     },
-    include: {
-      user: true,
-      items: {
-        include: {
-          product: true,
-        },
-      },
-    },
-  });
+  },
+});
 };
